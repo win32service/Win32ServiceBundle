e4d56cd3fb865affd14ef98c7b52069da9e4c158
jb cr <51637606+jbcr@users.noreply.github.com>
2024-07-25 17:50:11 +0200

    messenger work fine

---
+++ tests/Application/src/Event/TestMessage.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Win32ServiceBundle\Tests\Application\Event;
+
+final class TestMessage
+{
+    public function __construct(public string $message)
+    {
+    }
+}
